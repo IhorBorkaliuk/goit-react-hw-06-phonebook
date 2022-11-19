080e2a44677ba25b6437e9d4ccce1c742e13ec27
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-11-19 20:20:57 +0100

    mentors fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,38 @@ import { Filter } from './Filter/Filter';
 import { Title } from './ContactsList/ContactsListStyled';
 import { Form } from './Form/Form';
 import { AppWrapper } from './ContactsList/ContactsListStyled';
+import { ContactList } from './ContactsList/ContactsList';
 import { getStatusContacts } from 'redux/selectors';
+import { getStatusFilter } from 'redux/selectors';
 import { useSelector } from 'react-redux';
 import { Notify } from 'services/Notify';
 
 
+
 export function App() {
+  const filter = useSelector(getStatusFilter);
   const contacts = useSelector(getStatusContacts);
 
+  const getFilteredContacts = (filter, contacts) => {
+        if (!filter) {
+          return contacts;
+        }
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+    const filteredContacts = getFilteredContacts(filter, contacts);
   return (
     <AppWrapper>
       <Title>Phonebook</Title>
-      <Form/>
+      <Form />
       <Title>Contacts</Title>
       {contacts.length > 0 ? (
         <>
-          <Filter/>
+          <Filter />
+          <ContactList contacts={filteredContacts} />
         </>
       ) : (
         <Title>Додайте свій перший контакт до записної книжки</Title>
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,11 +1,8 @@
-import PropTypes from 'prop-types';
 import { LabelFilter, InputFilter, Wrapper } from './FilterStyled';
 
 import { getStatusFilter } from 'redux/selectors';
-import { getStatusContacts } from 'redux/selectors';
 import { useSelector, useDispatch } from 'react-redux';
 import { contactsFilter } from 'redux/filterSlice';
-import { ContactList } from 'components/ContactsList/ContactsList';
 
 
 
@@ -14,7 +11,6 @@ import { ContactList } from 'components/ContactsList/ContactsList';
 export const Filter = () => {
 
   const filter = useSelector(getStatusFilter)
-  const contacts = useSelector(getStatusContacts);
   const dispatch = useDispatch()
 
 
@@ -24,30 +20,12 @@ export const Filter = () => {
       dispatch(contactsFilter(value));
     };
 
-    const onFilter = () => {
-      if (!filter) {
-        return contacts;
-      }
-
-      const filterToLowerCase = filter.toLowerCase();
-
-      return contacts.filter(({ name }) =>
-        name.toLowerCase().includes(filterToLowerCase)
-      );
-  };
-  
-
   return (
     <Wrapper>
       <LabelFilter>
         Find contacts by name
         <InputFilter type="text" value={filter} onChange={handleChange} />
       </LabelFilter>
-      <ContactList contacts={onFilter()} />
     </Wrapper>
   );
-};
-
-Filter.propTypes = {
-  value: PropTypes.string,
-};
+}
